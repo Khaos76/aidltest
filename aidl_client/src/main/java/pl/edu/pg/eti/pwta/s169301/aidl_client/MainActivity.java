3b456e9a1e8c2ec7543c8055d8a8122c8093a40d
Khaos <mzablot@gmail.com>
2018-01-27 00:27:14 +0100

    dodano zamianę Implicit na Explicit Intent

--- aidl_client/src/main/java/pl/edu/pg/eti/pwta/s169301/aidl_client/MainActivity.java
+++ aidl_client/src/main/java/pl/edu/pg/eti/pwta/s169301/aidl_client/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -13,6 +15,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +28,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.multiply_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int num1 = Integer.parseInt(editVal1.getText().toString());
+                int num2 = Integer.parseInt(editVal2.getText().toString());
+                try {
+                    int result = calService.getResult(num1, num2);
+                    String msg = calService.getMessage(editName.getText().toString());
+                    resultView.setText(msg + result);
+                }catch (RemoteException e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -35,7 +57,7 @@ public class MainActivity extends Activity {
         resultView = findViewById(R.id.result);
         if (calService == null) {
             Intent it = new Intent("ms");
-            bindService(it, connection, Context.BIND_AUTO_CREATE);
+            bindService(convertImplicitIntentToExplicitIntent(it), connection, Context.BIND_AUTO_CREATE);
         }
     }
     @Override
@@ -44,22 +66,6 @@ public class MainActivity extends Activity {
         unbindService(connection);
     }
 
-    public void multiply(View v) {
-        switch (v.getId()){
-            case R.id.multiply_btn: {
-                int num1 = Integer.parseInt(editVal1.getText().toString());
-                int num2 = Integer.parseInt(editVal2.getText().toString());
-                try {
-                    int result = calService.getResult(num1, num2);
-                    String msg = calService.getMessage(editName.getText().toString());
-                    resultView.setText(msg + result);
-                }catch (RemoteException e){
-                    e.printStackTrace();
-                }
-                break;
-            }
-        }
-    }
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -75,4 +81,19 @@ public class MainActivity extends Activity {
                     .show();
         }
     };
+
+    public Intent convertImplicitIntentToExplicitIntent(Intent implicitIntent){
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> resolveInfoList = pm.queryIntentServices(implicitIntent, 0);
+
+        if(resolveInfoList == null || resolveInfoList.size() != 1) {
+            return null;
+        }
+        ResolveInfo serviceInfo = resolveInfoList.get(0);
+        ComponentName component = new ComponentName(serviceInfo.serviceInfo.packageName,
+                serviceInfo.serviceInfo.name);
+        Intent explicitIntent = new Intent(implicitIntent);
+        explicitIntent.setComponent(component);
+        return explicitIntent;
+    }
 }
